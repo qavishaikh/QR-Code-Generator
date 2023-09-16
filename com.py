6ba0c73f92bb22fd1006b548f031c743c4a1abc4
Qavi <qavi.shaikh11@gmail.com>
2023-09-16 12:46:38 +0500

    Company QR code

---
+++ com.py
@@ -0,0 +1,28 @@
+import qrcode
+
+# Function to generate a QR code
+def generate_qr_code(data, file_name):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
+
+    qr_code = qr.make_image(fill_color="black", back_color="white")
+    qr_code.save(file_name)
+
+if __name__ == "__main__":
+    data = """
+    REQUIREMENTS TO SELECT OUR COMPANY SERVICES
+    Knowledge of website how it works.
+    You should know where and how you will use your portfolio or website.
+    Necessary about Market pricing demand of Portfolios. 
+    Digital Web Creations - A Spark of Innovation Empowering Businesses Online
+    """
+    file_name = input("Enter the name of the QR code image file (e.g., qr_code.png): ")
+
+    generate_qr_code(data, file_name)
+    print(f"QR code saved as {file_name}")
